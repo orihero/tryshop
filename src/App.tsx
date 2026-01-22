41f1589b91cc077b8046ba52d50b9234d2926b1c
orihero <aka.orihero@gmail.com>
2026-01-23 01:04:42 +0500

    Refactor onboarding component to use new LiquidGlass package and enhance UI with color and size pickers. Update package dependencies and remove deprecated liquid-glass-react package. Add translation context for internationalization support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import { TranslationProvider } from '@/contexts/TranslationContext'
 import SplashScreen from '@/components/SplashScreen'
 import Onboarding from '@/components/Onboarding'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SplashScreen />} />
-      <Route path="/onboarding" element={<Onboarding />} />
-    </Routes>
+    <TranslationProvider>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+      </Routes>
+    </TranslationProvider>
   )
 }
 
